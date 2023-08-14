a503d291f92db8ff3ceb6b38f8ecfa022f9d5074
Michael Hammer <michael.p.hammer@gmail.com>
2023-08-14 07:13:56 -0400

    Update README and add files

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # Manim-Mobjects-and-Animations
+
+## Overview
+
+Manim is an open-srouce python library for generating videos of Mathematical Visualizations. 
+
+This repo contains code that showcases two of Manim's fundamental resources:
+1. MObjects - the objects that you can move around and transform
+2. Animations - the different things you can do to the MObjects (moving them around, transforming them, etc.)
+
+[mobjects.py](./mobjects.py) contains code that shows almost all of the mobjects. It shows them one after another with the name at the top. [animations.py](./animations.py) is the same but for almost all of the animations. 
+
+You can use the code as a reference for very simple examples. The Manim Community also has documentation on their website [here (Manim Community)](https://docs.manim.community/en/stable/reference.html). 
+
+
+## Watch Online
+
+You can view the videos created by each of these files on my [YouTube Channel](https://www.youtube.com/channel/UC3xlS5T2M6we94bCsYneNXA): 
+
+
+## Running the Code
+You need to have python and manim set up on your computer. Please search for documentation for [setting up Python](https://www.youtube.com/watch?v=M323OL6K5vs) and visit the [Manim Community website to install Manim](https://docs.manim.community/en/stable/installation.html).  
+
+Here are the versions I am using:  
+```
+$ python --version
+Python 3.10.0
+
+$ manim --version
+Manim Community v0.17.3
+```
+
+These commands will generate each of the videos if you run them in your Terminal:  
+```
+$ manim -pql mobjects.py
+$ manim -pql animations.py
+```
---
+++ animations.py
@@ -0,0 +1,114 @@
+from manim import * 
+import numpy as np
+
+class AllMObjects(ThreeDScene):
+    def construct(self):
+        t = Text("Manim Objects")
+        t.shift(UP*2.5)
+
+        mobject_list = [
+            # Geometry
+            Annulus(),
+            AnnularSector(),
+            Arc(),
+            Circle(),
+            Dot(),
+            Ellipse(),
+            Arrow(),
+            Vector(),
+            Square(),
+            Star(),
+            Triangle(),
+            ArrowCircleTip(),
+            
+            # Frame
+            FullScreenRectangle(),
+            ScreenRectangle(),
+
+            # Graph
+            Graph(vertices=(1,2,3,4,5), edges=[(1,2), (1,3), (4,2), (3,5), (1,5)]),
+
+            # Graphing
+            Axes(),
+            ComplexPlane(),
+            CoordinateSystem(),
+            NumberPlane(),
+            FunctionGraph(lambda x: (-1.0*x)**2),
+            NumberLine(),
+            BarChart(values=[1,2,3,4,5]),
+
+            # Logo
+            ManimBanner(),
+
+            # Matrix
+            DecimalMatrix([[1.0,2.0],[3.0,4.0]]),
+            IntegerMatrix([[1,2],[3,4]]),
+            Matrix([[1,2],[3,4]]),
+            MobjectMatrix([[Circle(),Square()],[Star(),Triangle()]]),
+
+            # Table
+            DecimalTable([[1,2,3,4], [5,6,7,8]], row_labels=[Text("Row 1"), Text("Row 2")]),
+            MathTable([["+", 0, 5, 10],[0, 0, 5, 10],[2, 2, 7, 12],[4, 4, 9, 14]],include_outer_lines=True),
+            MobjectTable( [ [Circle(), Square()] , [Triangle(), Star()] ] ),
+
+            # Text
+            Code(file_name="dummy.py", language="Python"),
+            DecimalNumber(5.5),
+            Integer(5),
+            BulletedList("Apple", "Pear", "Grape"),
+            MathTex(r"\frac{a}{b} = x^{2}"),
+            SingleStringMathTex(r"\frac{a}{b} = x^{2}"),
+            Tex("Hello World"),
+            Title("Here Is a Ttitle"),
+            MarkupText('<span foreground="red" size="x-large">Red text</span> is <i>fire</i>!"'),
+            Paragraph('There once was a man', 'that lived in a van', 'with only one right hand'),
+            Text("Here is Text"),
+
+            # Types
+            Point(color=RED),
+            PointCloudDot(),
+        ]
+
+        mobject_list_3d = [
+            ThreeDAxes(),
+            Dodecahedron(),
+            Icosahedron(),
+            Octahedron(),
+            Tetrahedron(edge_length=2),
+            Arrow3D(),
+            Cone(),
+            Cube(),
+            Cylinder(),
+            Dot3D(),
+            Line3D(start=np.array([-3,-3,-3]), end=np.array([3,3,3])),
+            Prism(),
+            Sphere(),
+            Torus(),
+        ]
+
+        self.add(t)
+        self.wait(0.2)
+        for mobj in mobject_list:
+            try:
+                self.play(Transform(t,Text(mobj.name).shift(UP*2.5)), FadeIn(mobj), run_time=0.5)
+                self.wait(0.5)
+                self.remove(mobj)
+            except:
+                continue
+        
+        self.play(Transform(t,Text("Looking at 3D Mobjects...").shift(UP*2.5)), run_time=0.5)
+        self.wait(1)
+
+        # self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.begin_3dillusion_camera_rotation(rate=4)
+        self.wait(2)
+
+        for mobj in mobject_list_3d:
+            try:
+                self.play(Transform(t,Text(mobj.name).shift(UP*2.5)), FadeIn(mobj), run_time=0.5)
+                self.wait(1)
+                self.remove(mobj)
+            except:
+                continue
+        self.play(FadeOut(t))
+        self.wait()
